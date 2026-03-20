83072ee83559fe6fce324be58393cc502b3de07e
pradeepmodakofficial@gmail.com <pradeepmodakofficial>
2026-03-21 01:05:26 +0530

    added public and protected route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,21 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { Toaster } from "react-hot-toast";
+import PublicRoute from "./components/publicRoute";
+import ProtectedRoute from "./components/protectedRoute";
+import SelectRole from "./pages/selectRole";
 const App = () => {
   return (
  <>
  <BrowserRouter>
  <Routes>
-  <Route path="/" element={<Home/>}/>
+  <Route element={<PublicRoute/>}>
   <Route path="/login" element={<Login/>}/>
+  </Route>
+    <Route element={<ProtectedRoute/>}>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/select-role" element={<SelectRole/>}/>
+  </Route>
  </Routes>
   <Toaster/>
  </BrowserRouter>
---
+++ frontend/src/pages/SelectRole.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { useAppData } from '../context/AppContext';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { authService } from '../main';
+
+type Role="customer"|"rider"|"seller"|null
+
+const selectRole = () => {
+    const [role,setRole]=useState<Role>(null);
+    const {setUser}=useAppData();
+    const navigate=useNavigate();
+
+    const roles:Role[]=["customer","rider","seller"];
+
+    const addRole=async()=>{
+        try {
+            const data=await axios.post(`${authService}/api/auth/add/role`,{role},{
+                headers:{
+                    Authorization:`Bearer ${localStorage.getItem("token")}`
+                }
+            });
+            localStorage.setItem("token",data.data.token);
+            setUser(data.data.user);
+            navigate("/",{replace:true});
+        } catch (error) {
+            console.log(error);
+            alert("Error adding role");
+        }
+    }
+  return (
+    <div className='flex min-h-screen items-center 
+    justify-center bg-white px-4'>
+
+    </div>
+  )
+}
+
+export default selectRole
