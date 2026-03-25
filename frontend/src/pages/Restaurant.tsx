7d88233321da949d77418b6757ebb99ea69e3d1c
pradeepmodakofficial@gmail.com <pradeepmodakofficial>
2026-03-25 18:38:09 +0530

    addmenuitems fronted frontend

--- frontend/src/pages/Restaurant.tsx
+++ frontend/src/pages/Restaurant.tsx
@@ -4,6 +4,8 @@ import { restaurantService } from "../main";
 import type { IRestaurant } from "../types";
 import AddRestaurant from "../components/AddRestaurant";
 import RestaurantProfile from "../components/RestaurantProfile";
+import MenuItems from "../components/MenuItems";
+import AddMenuItem from "../components/AddMenuItem";
 
 type SellerTab = "menu" | "add-item" | "sales";
 
@@ -75,8 +77,8 @@ const Restaurant = () => {
 
 
 <div className="p-5">
-    {tab === "menu" && <p>Menu Page</p>}
-    {tab === "add-item" && <p>Add Item Page</p>}
+    {tab === "menu" && <p><MenuItems/></p>}
+    {tab === "add-item" && <p><AddMenuItem/></p>}
     {tab === "sales" && <p>Sales Page</p>}
 </div>
 
