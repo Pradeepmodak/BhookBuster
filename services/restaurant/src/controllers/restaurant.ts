c7b6bf76bb54b53052ebeb2c48bfb90beea49a8c
pradeepmodakofficial@gmail.com <pradeepmodakofficial>
2026-03-27 01:30:13 +0530

    home page for restaurants created

--- services/restaurant/src/controllers/restaurant.ts
+++ services/restaurant/src/controllers/restaurant.ts
@@ -151,4 +151,59 @@ export const updateRestaurant = TryCatch(async (req: AuthenticatedRequest, res) 
         message:"Restaurant Updated",
         restaurant,
     })
-})
+});
+
+export const getNearbyRestaurant = TryCatch(async (req, res) => {
+  const { latitude, longitude, radius = 5000, search = "" } = req.query;
+
+  if (!latitude || !longitude) {
+    return res.status(400).json({
+      message: "Latitude and longitude are required",
+    });
+  }
+
+  const query: any = {
+    isVerified: true,
+  };
+
+  if (search && typeof search === "string") {
+  query.name = { $regex: search, $options: "i" }; // search by name (case-insensitive)
+  }
+const restaurants = await Restaurant.aggregate([
+  {
+    $geoNear: {
+      near: {
+        type: "Point",
+        coordinates: [Number(longitude), Number(latitude)],
+      },
+      distanceField: "distance",
+      maxDistance: Number(radius),
+      spherical: true,
+      query,
+    },
+},
+{
+  $sort: {
+    isOpen: -1,
+    distance: 1,
+  },
+},
+{
+  $addFields: {
+    distanceKm: {
+      $round: [{ $divide: ["$distance", 1000] }, 2],
+    },
+  },
+}
+]);
+res.json({
+    success:true,
+    count:restaurants.length,
+    restaurants,
+});
+});
+
+export const fetchSingleRestaurant = TryCatch(async (req, res) => {
+  const restaurant = await Restaurant.findById(req.params.id);
+  res.json(restaurant);
+});
