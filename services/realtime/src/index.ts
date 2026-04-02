b20b1cb42af24e87c0c3077b63041cebc1a9178d
pradeepmodakofficial@gmail.com <pradeepmodakofficial>
2026-04-02 18:55:10 +0530

    socket context added

---
+++ services/realtime/src/index.ts
@@ -0,0 +1,20 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import { initSocket } from "./socket.js";
+import internalRoutes from "./routes/internal.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+// parses incoming requests with JSON payloads and is based on body-parser
+app.use(express.json());
+app.use("./api/v1/internal",internalRoutes);
+const server = http.createServer(app);
+initSocket(server);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Realtime service is running port ${process.env.PORT}`);
+});
