f934fa0f4fda75094690e4ba8e15c9c4ab1c488b
Pradeep Modak <pradeepmodakofficial@gmail.com>
2026-04-06 00:30:43 +0530

    map feature added for everyone

--- frontend/src/components/UserOrderMap.tsx
+++ frontend/src/components/UserOrderMap.tsx
@@ -1,8 +1,89 @@
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+// react wrapper over leaflet maps 
+import * as L from "leaflet"; // core map library
+import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine"; // draws routes between points
+import { useEffect } from "react";
 
-const UserOrderMap = () => {
-  return (
-    <div>UserOrderMap</div>
-  )
+// leaflet manual typescript declaration
+declare module "leaflet" {
+  namespace Routing {
+    function control(options: any): any;
+    function osrmv1(options?: any): any;
+  }
+}
+const riderIcon = new L.DivIcon({
+  html: "🛵💨",
+  iconSize: [30, 30],
+  className: "",
+});
+const deliveryIcon = new L.DivIcon({
+  html: "🏠",
+  iconSize: [30, 30],
+  className: "",
+});
+const Routing = ({
+  from,
+  to,
+}: {
+  from: [number, number];
+  to: [number, number];
+}) => {
+  const map = useMap(); // access to leaflet map instance
+
+  // run when from and to changes
+  useEffect(() => {
+    const control = L.Routing.control({
+      waypoints: [L.latLng(from), L.latLng(to)],
+      lineOptions: {
+        styles: [{ color: "#E23744", weight: 5 }],
+      },
+      addWaypoints: false,
+      draggableWaypoints: false,
+      show: false,
+      createMarker: () => null,
+      router: L.Routing.osrmv1({
+        serviceUrl: "https://router.project-osrm.org/route/v1",
+      }),
+    }).addTo(map);
+
+    return () => {
+      map.removeControl(control);
+    };
+  }, [from, to, map]);
+
+  return null;
+};
+
+interface props {
+  riderLocation: [number, number];
+  deliveryLocation: [number, number];
 }
 
-export default UserOrderMap
+const UserOrderMap = ({
+  riderLocation,
+  deliveryLocation
+}: props) => {
+  return   <div className="rounded-xl bg-white shadow-sm p-3">
+      <MapContainer
+        center={riderLocation} // map centered towards riders loc
+        zoom={14}  // medium zoom ,city level view
+        className="h-87.5 w-full rounded-lg"
+      > 
+      {/* map ka background */}
+        <TileLayer
+          attribution="&copy; OpenStreetMap"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={riderLocation} icon={riderIcon}>
+          <Popup>Rider</Popup>
+        </Marker>
+       <Marker position={deliveryLocation} icon={deliveryIcon}>
+          <Popup>Delivery Location</Popup>
+        </Marker>
+        <Routing from={riderLocation} to={deliveryLocation}></Routing>
+      </MapContainer>
+    </div>
+};
+
+export default UserOrderMap;
